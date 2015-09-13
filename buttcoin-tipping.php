ac5048109bffc1faa3d1184b22bdac7c9499dc08
ElGatoSaez <geniointernet@gmail.com>
2015-09-13 01:33:21 -0300

    punto coma

--- buttcoin-tipping.php
+++ buttcoin-tipping.php
@@ -86,7 +86,7 @@ function add_buttcoin_tipping_icon($content)
 		
 		global $post;
 
-		$html = $html . "<div class='facebook'><a target='_blank' href='$fURL'>Dona acá</a></div>"
+		$html = $html . "<div class='facebook'><a target='_blank' href='$fURL'>Dona acá</a></div>";
 		
 		return $content = $content . $html;
 }
